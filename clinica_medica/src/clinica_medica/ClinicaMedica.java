dd0e2c88e46808fd717a33bbc6b04435cbd32f5f
Thiago Yasunaka <thiagoyasunaka@hotmail.com>
2019-10-24 09:05:10 -0300

    Crud implementado, necessário testar Crud de pacientes

--- clinica_medica/src/clinica_medica/ClinicaMedica.java
+++ clinica_medica/src/clinica_medica/ClinicaMedica.java
@@ -15,14 +15,25 @@ public class ClinicaMedica extends javax.swing.JFrame {
     private Secretary secretary;
     private List<Appointment> appointments = new ArrayList<Appointment>();
     private List<Patient> patients = new ArrayList<Patient>();
-    private Crud crud = new Crud();
 
     public ClinicaMedica() {
-        System.out.println(appointments);
         Patient patient = new Patient();
-        Appointment appointment = new Appointment(1, "22/12/1999", doctor, patient, true);
-        System.out.println(crud.addAppointment(appointment, appointments));
-        System.out.println(crud.getAppointment(2, appointments));
+        Appointment a1 = new Appointment(1, "22/12/1999", doctor, patient, true);
+        Appointment a2 = new Appointment(2, "22/12/2000", doctor, patient, true);
+        Appointment a3 = new Appointment(3, "22/12/2001", doctor, patient, true);
+        Appointment a4 = new Appointment(4, "22/12/2002", doctor, patient, true);
+        
+        appointments = Crud.addAppointment(a1, appointments);
+        appointments = Crud.addAppointment(a2, appointments);
+        appointments = Crud.addAppointment(a3, appointments);
+        appointments = Crud.addAppointment(a4, appointments);
+        
+        Appointment apNew = new Appointment(5, "22/12/2003", doctor, patient, true);
+        appointments = Crud.updateAppointment(4, apNew, appointments);
+        
+        for(Appointment a: appointments){
+            System.out.println(a.getDate());
+        }
         
         
         //initComponents();
--- clinica_medica/src/services/Crud.java
+++ clinica_medica/src/services/Crud.java
@@ -4,22 +4,33 @@ import java.util.List;
 import models.Patient;
 
 public class Crud {
-    public List<Appointment> removeAppointment(int appointmentId, List<Appointment> appointments){ 
-        return null;
+    
+    //AGENDAMENTOS
+    public static List<Appointment> removeAppointment(int appointmentId, List<Appointment> appointments){ 
+        List<Appointment> list = appointments;
+        list.remove(appointmentId);
+        return list;
     }
     
-    public List<Appointment> addAppointment(Appointment a, List<Appointment> apps){
+    public static List<Appointment> addAppointment(Appointment a, List<Appointment> apps){
         List<Appointment> list = apps;
         list.add(a);
         return list;
     }
     
-    public Appointment updateAppointment(Appointment appointment, List<Appointment> appointments){
-    
+    public static List<Appointment> updateAppointment(int appointmentId , Appointment appointment, List<Appointment> appointments){
+        List<Appointment> list = appointments;
+        for(int i=0; i<appointments.size(); i++){
+            if(list.get(i).getAppointmentId() == appointmentId){
+                list.remove(i);
+                list.add(i, appointment);
+                return list;
+            }
+        }
         return null;
     }
     
-    public Appointment getAppointment(int appointmentId, List<Appointment> appointments){
+    public static Appointment getAppointment(int appointmentId, List<Appointment> appointments){
         for(Appointment a: appointments){
             if(a.getAppointmentId() == appointmentId){
                 return a;
@@ -28,22 +39,40 @@ public class Crud {
         return null;
     }
     
-    public List<Patient> removePatient(int cpf, List<Patient> patients){
+    
+    //PACIENTES
+    public static List<Patient> removePatient(int cpf, List<Patient> patients){
+        List<Patient> pacientes = patients;
+        int index = 0;
+        for(Patient p: patients){
+            if(p.getCpf() == cpf){
+                pacientes.remove(index);
+                return pacientes;
+            }
+            index++;
+        }
         return null;
     }
     
-    public List<Patient> addPatient(Patient patient, List<Patient> patients){
+    public static List<Patient> addPatient(Patient patient, List<Patient> patients){
         List<Patient> p = patients;
         p.add(patient);
         return p;
     }
     
-    public Patient updatePatient(Patient patient, List<Patient> patients){
-        
+    public static List<Patient> updatePatient(int cpf, Patient patient, List<Patient> patients){
+        List<Patient> list = patients;
+        for(int i=0; i<list.size(); i++){
+            if(list.get(i).getCpf() == cpf){
+                list.remove(i);
+                list.add(i, patient);
+                return list;
+            }
+        }
         return null;
     }
     
-    public Patient getPatient(int cpf, List<Patient> patients){
+    public static Patient getPatient(int cpf, List<Patient> patients){
         for(Patient p: patients){
             if(p.getCpf() == cpf){
                 return p;
